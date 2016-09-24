78f39d601e70b6c0a461dc81bb0d0bc5684f5f58
Alex Moore-Niemi <moore.niemi@gmail.com>
2016-09-24 13:00:53 -0400

    indents

--- examples/comparing_canvas.rb
+++ examples/comparing_canvas.rb
@@ -2,27 +2,27 @@ require 'graph/function'
 
 file = File.expand_path('../comparing.html', __FILE__)
 Graph::Function.configure do |config|
-	config.terminal = 'canvas'
-	config.output = file
+  config.terminal = 'canvas'
+  config.output = file
 end
 
 def bubble_sort(array)
-	n = array.length
-	loop do
-		swapped = false
-		(n-1).times do |i|
-			if array[i] > array[i+1]
-				array[i], array[i+1] = array[i+1], array[i]
-				swapped = true
-			end
-		end
-		break if not swapped
-	end
-	array
+  n = array.length
+  loop do
+    swapped = false
+    (n-1).times do |i|
+      if array[i] > array[i+1]
+        array[i], array[i+1] = array[i+1], array[i]
+        swapped = true
+      end
+    end
+    break if not swapped
+  end
+  array
 end
 
 def sort(array)
-	array.sort
+  array.sort
 end
 
 puts "output: #{file}"
