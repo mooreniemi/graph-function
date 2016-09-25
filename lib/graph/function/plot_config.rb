8a4b9d6f2c70f3084666e468b4c78c4e0408dced
Alex Moore-Niemi <moore.niemi@gmail.com>
2016-09-25 15:45:05 -0400

    0.1.4

--- lib/graph/function/plot_config.rb
+++ lib/graph/function/plot_config.rb
@@ -2,7 +2,7 @@ module Graph
   module Function
     module PlotConfig
       def set_up(plot)
-        plot.ylabel 'execution time'
+        plot.ylabel 'execution time (seconds)'
         plot.xlabel 'input size'
         plot.terminal (t = Graph::Function.configuration.terminal)
         plot.output Graph::Function.configuration.output unless t == 'x11'
--- lib/graph/function/version.rb
+++ lib/graph/function/version.rb
@@ -1,5 +1,5 @@
 module Graph
   module Function
-    VERSION = '0.1.3'
+    VERSION = '0.1.4'
   end
 end
